4ddd03863a8ee363b1a0b77dad85477a645b737a
codewithsaidul <codewithsaidul@gmail.com>
2025-09-10 14:10:56 +0600

    hero section & navbar btn updated

--- src/components/HomePage/Hero.jsx
+++ src/components/HomePage/Hero.jsx
@@ -1,4 +1,4 @@
-import { Button } from "../ui/button";
+
 
 export default function Hero() {
   return (
@@ -18,12 +18,12 @@ export default function Hero() {
         </h1>
 
         <div className="mt-8 flex max-md:flex-col flex-row justify-center gap-4">
-          <Button className="py-4 px-6 bg-accent text-primary text-sm duration-700 hover:bg-accent/60 hover:duration-700 cursor-pointer">
+          <button className="py-4 px-6 rounded-lg bg-accent text-primary text-sm duration-700 hover:bg-accent/60 hover:duration-700 cursor-pointer">
             Our Vission
-          </Button>
-          <Button className="py-4 px-6 bg-transparent border border-accent text-accent text-sm duration-700 hover:bg-accent hover:text-primary hover:duration-700 cursor-pointer">
-            Explore Experties
-          </Button>
+          </button>
+          <button className="py-4 px-6 rounded-lg bg-transparent border border-accent text-accent text-sm duration-700 hover:bg-accent hover:text-primary hover:duration-700 cursor-pointer">
+            Explore Expertise
+          </button>
         </div>
       </div>
     </div>
