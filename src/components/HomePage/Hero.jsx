96e677c7f2965bb9632d9712a64535bdbc6591db
codewithsaidul <codewithsaidul@gmail.com>
2025-09-10 13:09:08 +0600

    hero section added

---
+++ src/components/HomePage/Hero.jsx
@@ -0,0 +1,31 @@
+import { Button } from "../ui/button";
+
+export default function Hero() {
+  return (
+    <div
+      style={{
+        background: `url("/images/hero.avif")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="w-full min-h-screen flex items-center justify-center relative filter saturate-130 contrast-100 px-4"
+    >
+      <div className="absolute inset-0 bg-black opacity-30"></div>
+      <div className="max-w-4xl w-full text-center relative z-10">
+        <h1 className="text-4xl sm:text-5xl lg:text-8xl text-white">
+          Timeless Comfort & Endless Luxury
+        </h1>
+
+        <div className="mt-8 flex max-md:flex-col flex-row justify-center gap-4">
+          <Button className="py-4 px-6 bg-accent text-primary text-sm duration-700 hover:bg-accent/60 hover:duration-700 cursor-pointer">
+            Our Vission
+          </Button>
+          <Button className="py-4 px-6 bg-transparent border border-accent text-accent text-sm duration-700 hover:bg-accent hover:text-primary hover:duration-700 cursor-pointer">
+            Explore Experties
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
